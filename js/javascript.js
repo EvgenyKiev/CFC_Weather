ece4d03f9663e2bb2521261b5788927d61c2a5a3
EvgenyKiev <padakluev@gmx.com>
2017-10-14 18:17:23 +0300

    1 commit

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,3 @@
-
-
-//start JQ
 $(document).ready(function() {
         //start gps
         if (!navigator.geolocation){
@@ -12,11 +9,8 @@ $(document).ready(function() {
             var latitude  = (position.coords.latitude).toFixed(2) ;
             var longitude =  ( position.coords.longitude).toFixed(2);
 
-
-            console.log('Latitude is ' + latitude + 'Longitude is ' + longitude )
-
+            //console.log('Latitude is ' + latitude + 'Longitude is ' + longitude );
             var apisrc = "https://fcc-weather-api.glitch.me/api/current?lat=" + latitude+ "&" + "lon=" + longitude;
-
 
             $.ajax({
                 type: 'get',
@@ -24,72 +18,43 @@ $(document).ready(function() {
                 async: true,
                 cache: false,
                 dataType: "json",
-
                 success: function(json) {
                     var html = "";
                     $(".name").html( JSON.stringify(json['name']));
-                    //
 
+                    var currT= (JSON.stringify(json.main['temp'])+' C');
+                    var currF= (JSON.stringify(json.main['temp']*1.8+32))+' F';
 
-                    //
+                    $(".temp").html(currT)
 
-
-
-                    var CurrT= JSON.stringify(json.main['temp'])+' C';
-                    var CurrF= ((JSON.stringify(json.main['temp']))*1.8 + 32)+' F'
-
-                    $(".temp").html(CurrT)
-;
-                    console.log(CurrF)
+                    //console.log((currF.toFixed(2)));
 
                     function modifyT() {
                         var t2 = document.getElementById("temp");
-                        if (t2.firstChild.nodeValue == CurrT) {
-                            t2.firstChild.nodeValue = CurrF;
+                        if (t2.firstChild.nodeValue == currT) {
+                            t2.firstChild.nodeValue = currF;
                         } else {
-                            t2.firstChild.nodeValue = CurrT;
+                            t2.firstChild.nodeValue = currT;
                         }
                     }
 
-                    // Добавляет слушателя событий для таблицы
                     var el = document.getElementById("temp");
                     el.addEventListener("click", modifyT, false);
+                    //
 
                     var imageLink=JSON.stringify(json.weather[0].icon);
-                    html += "<img src = " + imageLink + " " + "alt='" + 'Txt' + "'>"
-
+                    html += "<img src = " + imageLink + " " + "alt='" + 'Txt' + "'>";
                     $(".ico").html(html);
-
-
-
                 }
 
             });
 
-
-
-              ///test2
-            //// live coord
-
-        };
+        }
 
         function error() {
             console.log("Unable to retrieve your location")
-        };
+        }
 
-
-        //end gps
         navigator.geolocation.getCurrentPosition(success, error)
 
-
-
-
-//end JQ
-} )
-
-//
-// C ->F
-// T(°F) = T(°C) × 1.8 + 32
-//
-// F->C
-// T(°C) = (T(°F) - 32) / 1.8
+} );
